674e80b3541ad942dd3fac5e88e2b7a5afc4135d
Amin <amin030296@gmail.com>
2020-05-22 21:55:26 -0500

    hw4

--- assignments/hw-4/app.js
+++ assignments/hw-4/app.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const app = express();
-const notFoundPage = require("./middleware/error");
-const debug = require("debug")("app");
+const path = require("path");
+const bodyParse = require("body-parser");
 
-app.use(notFoundPage);
-app.use(express.json);
+app.use(bodyParse.urlencoded());
+app.use("/css", express.static(__dirname + "/node_modules/bootstrap/dist/css"));
 
-app.get("/", (req, res) => {
-  res.status(404);
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "home.html"));
 });
 
-console.log(process.env);
+app.use("/product", (req, res, next) => {
+  console.log(req.body); // { title: 'book' }
+  res.redirect("/");
+});
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  debug(`Localhost Started on port : ${port}`);
+  console.log("Locahost");
 });
--- assignments/hw-4/middleware/error.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-
-function notFoundPage(req, res, next) {
-  res.status(404).sendFile(path.join(__dirname, "../", "views", "404.html"));
-  next();
-}
-
-module.exports = notFoundPage;
